cd2a77953082f41e80a001aff5b2e454a7928a86
Shunichi Ikegami <sike.tm@gmail.com>
2018-11-16 01:22:11 +0900

    過去１週間分の配達分も表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@
 
 class HomeController < ApplicationController
   def index
+    @old_deliveries = Delivery.within(1.week.ago.to_date..Date.yesterday).default_order.preload(:items, :shop)
     @deliveries = Delivery.after(Date.current).default_order.preload(:items, :shop)
     @deliveries_without_delivery_date = Delivery.without_delivery_date.order(:id).preload(:items, :shop)
   end
--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -5,5 +5,6 @@ class Delivery < ApplicationRecord
 
   scope :without_delivery_date, -> { where(delivery_date: nil) }
   scope :after, -> (date) { where('delivery_date >= ?', date) }
+  scope :within, -> (date_range) { where(':date_begin <= delivery_date AND delivery_date <= :date_end', date_begin: date_range.begin, date_end: date_range.end)}
   scope :default_order, -> { order(:delivery_date) }
 end
